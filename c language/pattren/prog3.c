64057042ccf4439163511e22de58d3a2877d8c7c
c_language <bhavy0033@gmail.com>
2026-01-19 19:05:18 +0530

    19-01-2026

--- c language/pattren/prog3.c
+++ c language/pattren/prog3.c
@@ -1,20 +1,18 @@
-//12345
-//2345
-//345
-//45
-//5
+//54321
+//4321
+//321
+//21
+//1
 #include<stdio.h>
 int main()
 {
-    int i,j;
-
-    for(i=1;i<=5;i++)
+    for(int i=5;i>=1;i--)
     {
-        for(j=i;j<=5;j++)
+        for(int j=i;j>=1;j--)
         {
             printf("%d",j);
         }
         printf("\n");
     }
-    return 0;
+    
 }
--- c language/pattren/prog4.c
+++ c language/pattren/prog4.c
@@ -1,20 +1,17 @@
+//1
+//12
+//123
+//1234
 //12345
-//2345
-//345
-//45
-//5
 #include<stdio.h>
 int main()
 {
-    int i,j;
-
-    for(i=1;i<=5;i++)
+    for(int i=1;i<=5;i++)
     {
-        for(j=i;j<=5;j++)
+        for(int j=1;j<=i;j++)
         {
             printf("%d",j);
         }
         printf("\n");
     }
-    return 0;
 }
