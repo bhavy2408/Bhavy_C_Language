7298e3a644988b25434ee8b8172b07e3083baedc
c_language <bhavy0033@gmail.com>
2026-02-03 18:35:49 +0530

    03-02-2026

--- c language/Array/2D array/prog7.c
+++ c language/Array/2D array/prog7.c
@@ -3,5 +3,48 @@
 #include<stdio.h>
 int main()
 {
-    int a[10][10],b[10][10];
+    int a[10][10];
+    int i,j;
+    int m,n;
+
+    printf("Enter Rows=");
+    scanf("%d",&m);
+
+    
+    printf("Enter columns=");
+    scanf("%d",&n);
+
+    printf("Matrix:-\n");
+
+    for(i=0 ; i<m ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0 ; i<m ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Transpose matrix:-\n");
+
+    for(i=0 ; i<m ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
 }
---
+++ c language/Array/2D array/prog8.c
@@ -0,0 +1,15 @@
+//Count how many times an element appears in array.
+#include<stdio.h>
+int main()
+{
+    int a[10];
+    
+
+    printf("Enter your elements=");
+    scanf("%d",&a);
+
+    printf("%d",a);
+    
+    return 0;
+
+}
